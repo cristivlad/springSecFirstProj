288ff0afa126ac7840af826260c0743a78040b09
Cristiii <cristian.vlad10@gmail.com>
2023-04-13 20:58:56 +0300

    added userDetailsService and PasswordEncoder. Downgraded spring boot to 2.7

--- src/main/java/com/example/springsecfirstproj/controller/HelloController.java
+++ src/main/java/com/example/springsecfirstproj/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.springsecfirstproj;
+package com.example.springsecfirstproj.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
