2608f8f6dc09c9e929daed20e3c7986929686a73
Cristiii <cristian.vlad10@gmail.com>
2023-04-13 17:25:20 +0300

    init

---
+++ src/test/java/com/example/springsecfirstproj/SpringSecFirstProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecfirstproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecFirstProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
